931898d7aa8542270b18f1027e20ce1b0d6f559d
Adarsh077 <adarshco077@gmail.com>
2020-05-22 08:46:56 +0530

    minor chages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark info-color">
-        <Link className="navbar-brand" to="/">
-          Admin Panel
-        </Link>
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link
-              className="nav-link waves-effect waves-light"
-              to="/addcustomer"
-            >
-              Add Customer
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className='navbar navbar-expand-lg navbar-dark info-color'>
+				<Link className='navbar-brand' to='/'>
+					Admin Panel
+				</Link>
+				<ul className='navbar-nav ml-auto'>
+					<li className='nav-item'>
+						<Link
+							className='nav-link waves-effect waves-light'
+							to='/addcustomer'
+						>
+							Add Customer
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
